c66e02e7c5a0223225e614412274089ba41d64bf
DreamedWorker <demwre@outlook.com>
2022-09-01 18:24:56 +0800

    添加两个依赖库

--- src/main/java/org/dream/crafting/dcrafting/DCrafting.java
+++ src/main/java/org/dream/crafting/dcrafting/DCrafting.java
@@ -1,7 +1,12 @@
 package org.dream.crafting.dcrafting;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public final class DCrafting extends JavaPlugin {
 
@@ -17,5 +22,15 @@ public final class DCrafting extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         System.out.println("插件正在注销...");
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (label.equalsIgnoreCase("dreampath")){
+            sender.sendMessage(System.getProperty("user.dir") + "是你的工作路径");
+            //D:\MCServer是你的工作路径
+            return true;
+        }
+        return true;
     }
 }
